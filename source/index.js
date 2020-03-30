e0261e633fcc8365ca21d32b3d0c2f07f1365ee4
whosmeya <whosmeya@gmail.com>
2020-03-30 17:44:24 +0800

    feat: calcAverageCapital

--- source/index.js
+++ source/index.js
@@ -1,5 +1,8 @@
 import { plus, minus, times, divide } from 'number-precision';
 
+/**
+ * Average Capital Plus Interest（等额本息）
+ */
 function calcAverageCapitalPlusInterest({ amount, term, rate }) {
   // 月利率
   var monthlyRate = times(divide(rate, 12), 0.01);
@@ -9,16 +12,10 @@ function calcAverageCapitalPlusInterest({ amount, term, rate }) {
     times(
       amount,
       monthlyRate,
-      Math.pow(
-        plus(1, monthlyRate),
-        term
-      ),
+      Math.pow(plus(1, monthlyRate), term),
     ),
     minus(
-      Math.pow(
-        plus(1, monthlyRate),
-        term,
-      ),
+      Math.pow(plus(1, monthlyRate), term),
       1,
     )
   );
@@ -26,18 +23,19 @@ function calcAverageCapitalPlusInterest({ amount, term, rate }) {
   // 还款总额
   var totalPayment = times(monthlyPayment, term);
 
-  // 利息总额
+  // 还款总利息
   var totalInterest = minus(totalPayment, amount);
 
   // 每月还款明细
   var list = [];
-  var beginningBalance = amount;
+  var beginningBalance = amount;  // 所剩本金
   for (var i = 0; i < term; i++) {
     var monthlyInterest = times(beginningBalance, monthlyRate);
     var monthlyPrincipal = minus(monthlyPayment, monthlyInterest);
     var endingBalance = minus(beginningBalance, monthlyPrincipal);
     list.push({
-      key: i + 1,
+      period: i + 1,
+      monthlyPayment: monthlyPayment.toFixed(2),
       beginningBalance: beginningBalance.toFixed(2),
       interest: monthlyInterest.toFixed(2),
       principal: monthlyPrincipal.toFixed(2),
@@ -47,7 +45,47 @@ function calcAverageCapitalPlusInterest({ amount, term, rate }) {
   }
 
   return {
-    monthlyPayment: monthlyPayment.toFixed(2),
+    totalPayment: totalPayment.toFixed(2),
+    totalInterest: totalInterest.toFixed(2),
+    list,
+  }
+}
+
+/**
+ * Average Capital（等额本金）
+ */
+function calcAverageCapital({ amount, term, rate }) {
+  // 月利率
+  var monthlyRate = times(divide(rate, 12), 0.01);
+
+  // 每月所还本金
+  var monthlyPrincipal = divide(amount, term);
+
+  // 还款总利息
+  var totalInterest = times(plus(term, 1), amount, monthlyRate, 0.5);
+
+  // 还款总额
+  var totalPayment = plus(totalInterest, amount);
+
+  // 每月还款明细
+  var list = [];
+  var beginningBalance = amount;  // 所剩本金
+  for (var i = 0; i < term; i++) {
+    var monthlyInterest = times(beginningBalance, monthlyRate);
+    var monthlyPayment = plus(monthlyInterest, monthlyPrincipal)
+    var endingBalance = minus(beginningBalance, monthlyPrincipal);
+    list.push({
+      period: i + 1,
+      monthlyPayment: monthlyPayment.toFixed(2),
+      beginningBalance: beginningBalance.toFixed(2),
+      interest: monthlyInterest.toFixed(2),
+      principal: monthlyPrincipal.toFixed(2),
+      endingBalance: endingBalance.toFixed(2),
+    });
+    beginningBalance = endingBalance;
+  }
+
+  return {
     totalPayment: totalPayment.toFixed(2),
     totalInterest: totalInterest.toFixed(2),
     list,
@@ -55,8 +93,10 @@ function calcAverageCapitalPlusInterest({ amount, term, rate }) {
 }
 
 export {
-  calcAverageCapitalPlusInterest
+  calcAverageCapitalPlusInterest,
+  calcAverageCapital,
 }
 export default {
-  calcAverageCapitalPlusInterest
+  calcAverageCapitalPlusInterest,
+  calcAverageCapital,
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,21 +1,39 @@
-
-
 /**
  * Average Capital Plus Interest（等额本息）
  */
 export function calcAverageCapitalPlusInterest({ amount, term, rate }: {
   amount: number;  // 贷款金额
-  term: number;    // 贷款月数
+  term: number;    // 贷款期数（月数）
   rate: number;    // 年利率
 }): {
-  monthlyPayment: string;     // 月供
   totalPayment: string;       // 总还款
   totalInterest: string;      // 总利息
   list: {
-    key: number;              // key 期数
+    period: number;           // 第几期
+    monthlyPayment: string;   // 月供
     beginningBalance: string; // 当月还款前剩余本金
-    interest: string;         // 所还利息
-    principal: string;        // 所还本金
+    interest: string;         // 月供利息
+    principal: string;        // 月供本金
+    endingBalance: string;    // 当月还款后剩余本金
+  }[]
+};
+
+/**
+ * Average Capital（等额本金）
+ */
+export function calcAverageCapital({ amount, term, rate }: {
+  amount: number;  // 贷款金额
+  term: number;    // 贷款期数（月数）
+  rate: number;    // 年利率
+}): {
+  totalPayment: string;       // 总还款
+  totalInterest: string;      // 总利息
+  list: {
+    period: number;           // 第几期
+    monthlyPayment: string;   // 月供
+    beginningBalance: string; // 当月还款前剩余本金
+    interest: string;         // 月供利息
+    principal: string;        // 月供本金
     endingBalance: string;    // 当月还款后剩余本金
   }[]
 };
