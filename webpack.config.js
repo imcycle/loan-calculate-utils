5267d629146240e959ecc5229d772eacbc787e72
whosmeya <whosmeya@gmail.com>
2020-03-31 15:39:32 +0800

    feat: build:iife & build:iife:min

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
+const isMin = !!~process.env.NODE_ENV.indexOf('min');
+const type = process.env.NODE_ENV.replace(/:.*/g, '');
+const libraryTarget = type === 'iife' ? 'window' : type;
 
 const config = {
-  mode: process.env.NODE_ENV,
+  mode: isMin ? 'production' : 'development',
   entry: './source/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: process.env.NODE_ENV === 'production' ? 'index.min.js' : 'index.js',
-    library: '[name]',
-    libraryTarget: 'umd',
+    filename: `index.${type}${isMin ? '.min' : ''}.js`,
+    library: 'loanCalculateUtils',  // 整个库对外暴露的变量名
+    libraryTarget,  // 变量名添加到哪个上 browser
   },
   module: {
     rules: [
